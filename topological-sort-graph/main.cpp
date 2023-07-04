5e5b39c9972a0fa03b64e442d592e0203842e953
Suryansh <100128537+suryansh639@users.noreply.github.com>
2023-07-04 11:09:45 +0530

    Create main.cpp

---
+++ topological-sort-graph/main.cpp
@@ -0,0 +1,34 @@
+// also known as dfs but kahn algo if used bfs
+
+
+// put it in stack
+private:
+void dfs(int node , int vis[] , stack<int> &st, vector<int> adj[]){
+    vis[node]=1;
+    for(auto it: adj[node]){
+        if(!vis[it]) 
+        dfs(it,vis,st,adj);
+    }
+    st.push(node);
+    
+}
+
+
+public:
+vector<int> toposort(int V , vector<int>adj[]){
+    int vis[v] = {0};
+    stack<int> st;
+    for(int i=0;i<v;i++){
+        if(!vis[i]){
+            dfs(i , vis , st , adj);
+        }
+    }
+    vector<int> ans;
+    while(!st.empty()){
+        ans.push_back(st.top());
+        st.pop();
+        
+    }
+    return ans;
+    
+}
